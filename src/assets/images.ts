27daa81b592e1e847108e2dd3c44890d047a83f3
Miguel Malagon <mmalagon@homecityice.com>
2025-04-23 23:55:38 -0600

    Adds a smaller logo to the navbar
    Uses a smaller logo image in the navbar to improve visual appeal and responsiveness on smaller screens.

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,5 +1,7 @@
 import Logo from './img/logo.png';
+import LogoSmall from './img/Logo3.png';
 
 export const images = {
   logo: Logo,
+  logoSmall: LogoSmall,
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,32 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import { Box, Container, Flex, Text } from '@radix-ui/themes';
 import Link from 'next/link';
+import { images } from '@/assets/images';
 
 export default function Navbar() {
   return (
-    <Box 
-      asChild 
-      py="3" 
+    <Box
+      asChild
+      py="3"
       className="fixed top-0 left-0 right-0 z-50 bg-white border-b border-gray-200"
     >
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-18 overflow-hidden">
         <Container>
           <Flex justify="between" align="center">
-            <Link href="/">
-              <Text size="5" weight="bold">
-                MyApp
-              </Text>
+            <Link href="/" className=" overflow-hidden">
+              <Image
+                className="w-[60px] h-[60px]"
+                src={images.logoSmall}
+                alt="logo"
+              />
             </Link>
-
             <Flex gap="3">
               <Link href="/sign-in">
                 <Button variant="secondary">Login</Button>
               </Link>
-              <Link href="/register">
+              <Link href="/sign-up">
                 <Button variant="default">Register</Button>
               </Link>
             </Flex>
